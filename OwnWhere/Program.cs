413f34dded07851aa200cc0dbb4553751cd4d79f
olegshok <pastushok91@gmail.com>
2022-12-11 23:13:04 +0200

    Add solution OwnWhere and file Program.cs with method MyWhere analog .Where

---
+++ OwnWhere/Program.cs
@@ -0,0 +1,26 @@
+﻿var list = new List<int> { 1, 2, 3, 4 };
+
+var oddQuery = list.MyWhere(i => i % 2 != 0); // MyWhere - ваш метод
+
+list.Add(5);
+var filterredList = oddQuery.ToList(); // 1, 3, 5
+
+foreach (int item in filterredList)
+{
+    Console.WriteLine(item);
+}
+
+public static class LinqExtensionMethod
+{
+    public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> list, Func<TSource, bool> filterEvenNumbers)
+    {
+        foreach (TSource item in list)
+        {
+            if (filterEvenNumbers(item))
+            {
+                yield return item;
+            }
+        }
+    }
+}
+
